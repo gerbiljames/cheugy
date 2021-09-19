43ba8b56a2b98191fae39ed6a0673baab8555870
James White <james.white@urbanthings.co>
2021-09-19 20:16:42 +0100

    Install more dependencies

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def health_check():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 from cheugy import bot
